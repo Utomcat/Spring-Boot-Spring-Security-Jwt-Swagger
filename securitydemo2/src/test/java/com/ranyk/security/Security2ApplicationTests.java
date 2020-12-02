fc72f0fd046ac94f10e543304487195958607c57
ranyk <1390851149@qq.com>
2020-12-02 13:51:57 +0800

    修改启动端口

--- securitydemo2/src/test/java/com/ranyk/security/Security2ApplicationTests.java
+++ securitydemo2/src/test/java/com/ranyk/security/Security2ApplicationTests.java
@@ -1,13 +1,25 @@
 package com.ranyk.security;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
 
 @SpringBootTest
 class Security2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		Map<String, Object> params = Maps.newHashMap();
+		params.put("keyword","维修");
+		params.put("id",null);
+		params.put("regionCodes",null);
+
+		System.out.println(JSON.toJSONString(params));
+
 	}
 
 }
